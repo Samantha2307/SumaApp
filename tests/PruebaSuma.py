38508ccc652bad8429fc6606fdb9f100d67cd927
Samantha2307 <72857554@continental.edu.pe>
2022-04-12 23:42:09 -0500

    Se modifico el caso de prueba de test_operacionSuma_dosNumerosPositivos_retornaSuma

--- tests/PruebaSuma.py
+++ tests/PruebaSuma.py
@@ -11,8 +11,8 @@ class PruebaSuma(unittest.TestCase):
     def test_operacionSuma_dosNumerosPositivos_retornaSuma(self):
         # Arrange
         self.sumando1=9
-        self.sumando2=4
-        self.resultadoesperado=13
+        self.sumando2=50
+        self.resultadoesperado=69
 
         # Do
         self.resultadoActual=self.suma.operacionSuma(self.sumando1,self.sumando2)
